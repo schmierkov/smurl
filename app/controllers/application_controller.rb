73ad5cf7ba3474d2208171c6e62a64df2c9c9b6b
Sirko Sittig <sirko.sittig@gmail.com>
2015-03-16 23:54:59 +0100

    change method name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from ActionController::ParameterMissing, with: :not_found
+  rescue_from ActionController::ParameterMissing, with: :redirect_to_root
 
   private
 
-  def not_found
+  def redirect_to_root
     redirect_to root_url
   end
 end
