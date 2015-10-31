dfe0978466783fe01b5f917dcadc35adfd0dea8d
Sirko Sittig <sirko.sittig@gmail.com>
2015-10-31 19:52:11 -0300

    allow all origins -_-

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Smurl
   class Application < Rails::Application
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*'
+    })
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
